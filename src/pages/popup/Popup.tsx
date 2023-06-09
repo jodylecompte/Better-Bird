ba3103fcc6f62d4b2b86d15adf1ec6dcef8ad664
Jody LeCompte <jody@jodylecompte.com>
2023-06-09 16:31:26 -0500

    Import boilerplate

---
+++ src/pages/popup/Popup.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import logo from "@assets/img/logo.svg";
+import "@pages/popup/Popup.css";
+
+const Popup = () => {
+  return <div className="App">Test</div>;
+};
+
+export default Popup;
